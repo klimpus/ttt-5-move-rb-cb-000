c27a56e9e04a03b430f75cd24d47a50bf47f7b17
Kristopher Limpus <krislimpus@gmail.com>
2018-04-13 01:36:47 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
+def input_to_index(user_input)
+  puts "Where would you like to go?"
+    user_input = gets.string.to_i
+end

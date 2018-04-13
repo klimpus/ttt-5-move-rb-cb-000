126a64bd54e7210dd7e9e2ce0c346935792dcc80
Kristopher Limpus <krislimpus@gmail.com>
2018-04-13 01:38:41 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,6 +8,7 @@ end
 
 def input_to_index(user_input)
   puts "Where would you like to go?"
-    user_input = gets.string.to_i - 1
-    user_input
+    user_input = gets.string.to_i
+    converted_input = user_input - 1
+    
 end
